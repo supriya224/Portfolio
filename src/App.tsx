42972886b885d32f9ce8b1c68dd4337736a9bc20
supriya224 <52038704+supriya224@users.noreply.github.com>
2026-09-03 10:03:11 +0530

    resolved build issues

--- src/App.tsx
+++ src/App.tsx
@@ -9,24 +9,8 @@ import Finora from "./components/Finora";
 import SelectedWork from "./pages/Selected.Page";
 import FoodSafety from "./components/Food";
 import AboutPage from "./pages/About.page";
-import { FloatingCards } from "./components/FloatingCards";
 
 
-const cards = [
-  {
-    id: "one",
-    content: <div style={{ padding: 24 }}>First card</div>,
-  },
-  {
-    id: "two",
-    alt: true,
-    content: <div style={{ padding: 24 }}>Second card</div>,
-  },
-  {
-    id: "three",
-    content: <div style={{ padding: 24 }}>Third card</div>,
-  },
-];
 
 function App() {
   return (
@@ -42,12 +26,7 @@ function App() {
         <Route path="/finora" element={<Finora />} />
         <Route path="/food" element={<FoodSafety />} />
         <Route path="/about" element={<AboutPage />} />
-                <Route path="/light" element={<FloatingCards  cards={cards}
-      gap={16}
-      radius={12}
-      floatDistance={10}
-      speed={1.1}
-      tilt={9} />} />
+  
       </Routes>
     </>
   );
